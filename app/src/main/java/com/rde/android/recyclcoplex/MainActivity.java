8e4369708dce7c767662a859b6678a8d6e7f439b
Rodolfo d'Ettorre <rodolfothegreat@hotmail.com>
2019-12-08 23:50:54 +1100

    Creating project

---
+++ app/src/main/java/com/rde/android/recyclcoplex/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rde.android.recyclcoplex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
